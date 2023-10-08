89f69b9a225d4a503510274700183ddb848bc010
ReasonZx <xico.reasonzx@gmail.com>
2023-10-08 15:28:13 +0200

    Dapp token SC

---
+++ contracts/DappToken.sol
@@ -0,0 +1,11 @@
+// contracts/customToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DappToken is ERC20 {
+    constructor() ERC20("Dapp Coin", "DAPP") {
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
